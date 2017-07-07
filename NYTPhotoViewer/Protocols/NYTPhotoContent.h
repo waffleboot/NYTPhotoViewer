f41cb5e066dc2026645f43320fe19a54f3719a27
Andrei Yangabishev <waffleboot@gmail.com>
2017-07-07 17:49:53 +0300

    добавил NYTPhotoContent

---
+++ NYTPhotoViewer/Protocols/NYTPhotoContent.h
@@ -0,0 +1,13 @@
+//
+//  NYTPhotoContent.h
+//  NYTPhotoViewer
+//
+//  Created by Андрей on 07.07.17.
+//  Copyright © 2017 NYTimes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NYTPhotoContent <NSObject>
+
+@end
